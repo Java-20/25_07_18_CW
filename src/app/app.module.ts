c004b5bc4e94095105d40b6fa8abbc8d1e74695a
Gregory Sheygam <sheygam@gmail.com>
2018-07-27 00:08:31 +0300

    classwork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemFormComponent } from './item-form/item-form.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemRowComponent } from './item-row/item-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemFormComponent,
+    ItemListComponent,
+    ItemRowComponent
   ],
   imports: [
     BrowserModule
